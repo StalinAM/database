1011d446a913276457c3c06d6df822139af829ce
Stalin <sta_vla411@hotmail.com>
2024-06-20 20:38:08 -0500

    remove getData function

--- driver-mongo/index.js
+++ driver-mongo/index.js
@@ -1,25 +1,14 @@
 import { getConnection } from './mongo.js'
 import fs from 'node:fs'
 
-const getData = async () => {
-  try {
-    const connection = await getConnection()
-    const data = await connection.collection('collection').find().toArray()
-    console.log(data)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
 const data = JSON.parse(
   fs.readFileSync('personajes_marvel_subir.json', 'utf-8')
 )
-// console.log(data.results)
 const insertData = async () => {
   try {
     const connection = await getConnection()
     const result = await connection
-      .collection('collection')
+      .collection('marvel')
       .insertMany(data.results)
     console.log(result)
   } catch (error) {
