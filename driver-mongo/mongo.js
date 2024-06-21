d67abd1a7659a1a7cecc9082c7c2387005aa8f52
Stalin <sta_vla411@hotmail.com>
2024-06-20 20:38:26 -0500

    change name database

--- driver-mongo/mongo.js
+++ driver-mongo/mongo.js
@@ -2,9 +2,10 @@ import { MongoClient } from 'mongodb'
 
 const getConnection = async () => {
   try {
-    const mongoUrl = 'mongodb://localhost:27017/phonebook'
+    const mongoUrl =
+      'mongodb+srv://<user>:<password>@cluster0.ldhoocb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     const client = await MongoClient.connect(mongoUrl)
-    return client.db()
+    return client.db('prueba')
   } catch (error) {
     console.error(error)
   }
