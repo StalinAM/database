c210b8e3129971128d9d8bac3c8e50b7faeb3f82
StalinAM <sta_vla411@hotmail.com>
2024-06-30 23:51:41 -0500

    add client

--- driver-casandra/index.js
+++ driver-casandra/index.js
@@ -1,1 +1,17 @@
-import fs from 'node:fs'
+import { Client } from 'cassandra-driver'
+
+const client = new Client({
+  contactPoints: ['localhost'], // Replace with your Cassandra contact points
+  localDataCenter: 'datacenter1', // Replace with your Cassandra data center
+  keyspace: 'mykeyspace' // Replace with your keyspace name
+})
+
+client
+  .connect()
+  .then(() => {
+    console.log('Connected to Cassandra')
+    // Your code here
+  })
+  .catch((error) => {
+    console.error('Error connecting to Cassandra', error)
+  })
